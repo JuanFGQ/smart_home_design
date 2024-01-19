2ac530ffa596b12a9db88ad3582be1d8e5dec951
juanFGQ <juanfelipegq@outlook.es>
2024-01-19 08:04:07 +0100

    assets

---
+++ lib/smart_home2/widgets/video_player.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoPlayerController extends StatefulWidget {
+  const VideoPlayerController({super.key});
+
+  @override
+  State<VideoPlayerController> createState() => _VideoPlayerControllerState();
+}
+
+class _VideoPlayerControllerState extends State<VideoPlayerController> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
